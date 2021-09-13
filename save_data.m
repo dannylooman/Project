e4ce58f5bb4fca541d9c40acb82306215b3278d7
dannylooman <d.looman@student.tudelft.nl>
2021-09-13 14:37:28 +0200

    added 100 hz dataset

--- save_data.m
+++ save_data.m
@@ -1,4 +1,4 @@
 % Sensor saving script
 
-filename = replace(strcat("saved_data\", datestr(datetime), "-second_link_swing.mat"),':','_');
+filename = replace(strcat("saved_data\", datestr(datetime), "-second_link_swing_100hz.mat"),':','_');
 save(filename, 'theta1', 'theta2', 'input')
