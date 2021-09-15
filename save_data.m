3a4ee864f04b3a4481e5d25c9f82c2291b1f77b9
dannylooman <d.looman@student.tudelft.nl>
2021-09-15 11:12:13 +0200

    hjk

--- save_data.m
+++ save_data.m
@@ -1,4 +1,4 @@
 % Sensor saving script
 
-filename = replace(strcat("saved_data\", datestr(datetime), "-block_signal_stationary_second_link.mat"),':','_');
+filename = replace(strcat("saved_data\", datestr(datetime), "-second_link_fast.mat"),':','_');
 save(filename, 'theta1', 'theta2', 'input')
