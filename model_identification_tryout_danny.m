bde3544e0a1b8ee3cc66b7622ec4203cc5f183d9
dannylooman <d.looman@student.tudelft.nl>
2021-09-20 13:48:44 +0200

    added derivative to model identification

--- model_identification_tryout_danny.m
+++ model_identification_tryout_danny.m
@@ -10,17 +10,19 @@ opt.SearchOptions.MaxIterations = 50;
 %% Create Identification data
 load("saved_data\07-Sep-2021 11_44_42.mat");
 Ts = input.time(2) - input.time(1);
+theta1 = conv_theta(theta1); %Preprocess theta1
+theta2 = conv_theta(theta2); %Preprocess theta1
 
 % Transform timeseries to arrays and shrink input array to same size as
-% output array, multiply with -1 makes it better?
-y = [unwrap(theta1.Data), unwrap(theta2.Data)];
-u = input.Data(1:length(theta1.Data), :);
+% output array
+y = [theta1.angle, theta2.angle, theta1.dot, theta2.dot];
+u = input.Data(1:length(theta1.angle), :);
 
-z = iddata(y, u, Ts, 'Name', 'RotPendulum', 'Inputunit', 'V', 'OutputUnit', ['rad'; 'rad']);
+z = iddata(y, u, Ts, 'Name', 'RotPendulum');
 
 %% Create grey box model
 file_name = 'model_function_file';
-Order = [2 1 5];  % 2 outputs, 1 input, 5 states
+Order = [4 1 5];  % 4 outputs, 1 input, 5 states
 
 Parameters = [struct('Name', 'L1',  'Value', 0.1000, 'Unit', 'm',     'Minimum', 0.05,  'Maximum', 0.15, 'Fixed', true);
               struct('Name', 'L2',  'Value', 0.0884, 'Unit', 'm',     'Minimum', 0.05,  'Maximum', 0.15, 'Fixed', true);
@@ -34,11 +36,11 @@ Parameters = [struct('Name', 'L1',  'Value', 0.1000, 'Unit', 'm',     'Minimum',
               struct('Name', 'L_a', 'Value', 3e-06, 'Unit', 'H',     'Minimum', 0.00,  'Maximum', 5.00, 'Fixed', true);
               ];
 
-InitialStates = [struct('Name', 'x1', 'Value', theta1.Data(1), 'Unit', 'rad',   'Minimum', -pi, 'Maximum', pi, 'Fixed', true);
-                 struct('Name', 'x2', 'Value', theta2.Data(1), 'Unit', 'rad',   'Minimum', -pi, 'Maximum', pi, 'Fixed', true);
-                 struct('Name', 'x3', 'Value', 0.0, 'Unit', 'A',     'Minimum', -0.5, 'Maximum', 0.5, 'Fixed', true);
-                 struct('Name', 'x4', 'Value', 0.0, 'Unit', 'rad/s', 'Minimum', -0.5, 'Maximum', 0.5, 'Fixed', true);
-                 struct('Name', 'x5', 'Value', 0.0, 'Unit', 'rad/s', 'Minimum', -0.5, 'Maximum', 0.5, 'Fixed', true);
+InitialStates = [struct('Name', 'x1', 'Value', theta1.angle(1), 'Unit', 'rad',   'Minimum', -pi, 'Maximum', pi, 'Fixed', true);
+                 struct('Name', 'x2', 'Value', theta2.angle(1), 'Unit', 'rad',   'Minimum', -pi, 'Maximum', pi, 'Fixed', true);
+                 struct('Name', 'x3', 'Value', 0.0, 'Unit', 'A',     'Minimum', -0.5, 'Maximum', 0.5, 'Fixed', false);
+                 struct('Name', 'x4', 'Value', 0.0, 'Unit', 'rad/s', 'Minimum', -0.5, 'Maximum', 0.5, 'Fixed', false);
+                 struct('Name', 'x5', 'Value', 0.0, 'Unit', 'rad/s', 'Minimum', -0.5, 'Maximum', 0.5, 'Fixed', false);
                 ];
 Sample_time = 0; % Continuous model
 
