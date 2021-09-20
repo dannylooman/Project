3b777667f07376107d9394f781d93f4e4467eae5
dannylooman <d.looman@student.tudelft.nl>
2021-09-20 11:28:57 +0200

    added seccond measurment to dcmotor identification

--- model_identification_dcmotor.m
+++ model_identification_dcmotor.m
@@ -10,22 +10,20 @@ opt.SearchOptions.MaxIterations = 50;
 %% Create Identification data
 load("saved_data\15-Sep-2021 09_23_40-freq_sweep_stationary_second_link.mat");
 Ts = input.time(2) - input.time(1);
+theta1 = conv_theta(theta1); %Preprocess theta1
 
 % Transform timeseries to arrays and shrink input array to same size as
-% output array, multiply with -1 makes it better?
-y = conv_theta(theta1).data;
-% y = unwrap(theta1.Data);
-u = input.Data(1:length(theta1.Data), :);
+% output array
+y = [theta1.angle, theta1.dot];
+u = input.Data(1:length(theta1.angle), :);
 
-z = iddata(y, u, Ts, 'Name', 'RotPendulum', 'Inputunit', 'V', 'OutputUnit', 'rad');
+z = iddata(y, u, Ts, 'Name', 'RotPendulum');
 
 %% Subspace Identification
 subspace_id_sys = n4sid(z,3,'Ts',0,'Form', 'canonical');
 
 %% Create grey box model
-% J, b, K, L, R
 file_name = 'dc_motor_model';
-Order = [1 1 3];  % 1 outputs, 1 input, 3 states
 
 J = 0.005;
 b = 0;
@@ -35,13 +33,12 @@ R = 0.08;
 
 Parameters = {'inertia',J; 'friction',b; 'motor_constant',K; 'L',L; 'R', R};
 
-
-init_sys = idgrey(file_name, Parameters, 'c', 'Name', 'Rotational Pendulum');
-init_sys.Structure.Parameters(1).Free = false;
-init_sys.Structure.Parameters(2).Free = false;
-init_sys.Structure.Parameters(3).Free = false;
-init_sys.Structure.Parameters(4).Free = false;
-init_sys.Structure.Parameters(5).Free = false;
+init_sys = idgrey(file_name, Parameters, 'c');
+init_sys.Structure.Parameters(1).Free = true;
+init_sys.Structure.Parameters(2).Free = true;
+init_sys.Structure.Parameters(3).Free = true;
+init_sys.Structure.Parameters(4).Free = true;
+init_sys.Structure.Parameters(5).Free = true;
 
 %% Run identification
 identified_system = greyest(z, init_sys, opt);
